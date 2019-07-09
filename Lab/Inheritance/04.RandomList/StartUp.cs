e26cf52ba51ccf93bc820c6088f54a83c5502385
Yoana <marinova.m.y@gmail.com>
2019-07-09 13:28:47 +0300

    Inheritance.sln

---
+++ Lab/Inheritance/04.RandomList/StartUp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CustomRandomList
+{
+   public class StartUp
+    {
+        public static void Main()
+        {
+            RandomList randomList = new RandomList()
+            {
+                "Yoana",
+                "Zlatina",
+                "Atanas"
+            };
+
+            Console.WriteLine(randomList.RandomString());
+        }
+    }
+}
---
+++ Lab/Inheritance/05.StackOfStrings/StartUp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CustomStack
+{
+   public class StartUp
+    {
+       public static void Main()
+        {
+            StackOfStrings stack = new StackOfStrings();
+
+            stack.Push("kak");
+
+            StackOfStrings stack2 = new StackOfStrings();
+
+            stack.AddRange(stack2);
+        }
+    }
+}
