f88af1739355cb9ae67d08edc7437134cffdd5c2
Yoana <marinova.m.y@gmail.com>
2019-07-10 18:32:04 +0300

    Inheritance - ex.1

---
+++ Excersice/Inheritance/01.Person/Person.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Person
+{
+    public class Person
+    {
+        private string name;
+        private int age;
+
+        public Person(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+
+        public string Name
+        {
+            get => this.name;
+            set => this.name = value;
+        }
+        public int Age
+        {
+            get => this.age;
+            set => this.age = value;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append(String.Format("Name: {0}, Age: {1}",
+                this.Name,
+                this.Age));
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
