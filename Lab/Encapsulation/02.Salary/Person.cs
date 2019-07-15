c7a1f6a467940ebf62742431b8abe16a1a4d4849
Yoana <marinova.m.y@gmail.com>
2019-07-15 12:41:14 +0300

    Encapsulation.sln - Ex.3 AND Ex.3

---
+++ Lab/Encapsulation/02.Salary/Person.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonsInfo
+{
+    public class Person
+    {
+        public Person(string firstName, string lastName, int age, decimal salary)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Age = age;
+            this.Salary = salary;
+        }
+
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+        public int Age { get; private set; }
+        public decimal Salary { get; set; }
+
+        public void IncreaseSalary(decimal percentage)
+        {
+            decimal bonus = this.Age < 30 ? percentage / 200 : percentage / 100;
+
+            this.Salary += this.Salary * bonus;
+        }
+
+        public override string ToString()
+        {
+            return $"{this.FirstName} {this.LastName} receives {this.Salary:F2} leva.";
+        }
+    }
+}
