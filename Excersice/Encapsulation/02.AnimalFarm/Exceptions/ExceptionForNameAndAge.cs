0bb9997f00d39875860d20f19d52df6526080f44
Yoana <marinova.m.y@gmail.com>
2019-07-16 10:13:08 +0300

    Ex.2

---
+++ Excersice/Encapsulation/02.AnimalFarm/Exceptions/ExceptionForNameAndAge.cs
@@ -0,0 +1,11 @@
+﻿namespace AnimalFarm.Exceptions
+{
+   public static class ExceptionForNameAndAge
+    {
+        public static string NameCannotBeEmptyException =
+            "Name cannot be empty.";
+
+        public static string AgeBeetwenZeroAndFifteenException =
+            "Age should be between 0 and 15.";
+    }
+}
